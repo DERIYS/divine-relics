52c70e8d483964b7650a5e2319053458b37db199
DERIYS <leva.tasher228@gmail.com>
2024-04-07 18:21:05 +0200

    Added Motosignir (no functionality)

---
+++ src/main/java/com/deriys/tutorialmod/items/ModItems.java
@@ -0,0 +1,22 @@
+package com.deriys.tutorialmod.items;
+
+import com.deriys.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MODID);
+
+    public static final RegistryObject<Item> MOTOSIGNIR = ITEMS.register("motosignir",
+            () -> new Item(new Item.Properties().fireResistant().tab(CreativeModeTab.TAB_COMBAT)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
